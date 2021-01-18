4c43c856a85b304e3d3976745fdc59d0085b920a
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-17 23:28:57 -0600

    Adds title line to routine#new pg. Adds new and create method (exercise).

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,21 @@
 class ExercisesController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+        @exercise = Exercise.new(exercise_params)
+        if @exercise.save
+            redirect_to exercise_path(@exercise)
+        else
+            render :new
+        end
+    end
+
+    private 
+
+    def exercise_params
+        params.require(:exercise).permit(:name, :target_area, :category)
+    end
 end
