649691ff71e8eaef8f1355eb0de221415218ee8f
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-18 13:27:39 -0600

    Eliminates routine#new pg. Adds nested form for new workout.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,10 @@
 class WorkoutsController < ApplicationController
+
+    def new
+        @workout = Workout.new
+        5.times { @workout.routines.build }
+    end
+
+    def create
+    end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,6 @@ class Workout < ApplicationRecord
     belongs_to :user
     has_many :routines
     has_many :exercises, through: :routines
+
+    accepts_nested_attributes_for :routines
 end
