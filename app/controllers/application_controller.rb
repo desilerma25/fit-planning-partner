8f066fa9dd23bc5cca4526ff3995d5353c860e80
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-17 21:45:29 -0600

    Adds helper methods redirect_if_not_logged, logged_in?, current_user. Fixes error on User show page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+include ApplicationHelper
 
     def user_params
         params.require(:user).permit(:name, :email, :password)
     end
-    
+
+    def redirect_if_not_logged_in
+        redirect_to "/" if !logged_in?
+    end
+
 end
