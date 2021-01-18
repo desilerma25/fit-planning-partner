7431145589d6c49fd507c141d8b2019511f9bee1
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-17 21:37:40 -0600

    Adds login/signup/logout methods. Adds links to layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
+    
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,22 @@ class SessionsController < ApplicationController
     end
 
     def new
+        @user = User.new
     end
 
-    def create
+    def create 
+        user = User.find_by(email:params['user']['email'])
+        if user && user.authenticate(params['user']['password'])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else 
+            flash[:message] = "Email or Password does not match our records. Please try again."
+            render :new
+        end
     end
 
     def destroy
+        session.delete :user_id
+        redirect_to :root
     end
 end
