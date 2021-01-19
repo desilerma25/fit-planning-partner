580dbc10ade6066b2f56770de0b8677ac0297520
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-18 20:07:07 -0600

    Removes gem rack-mini-profiler. Adds create action to Workouts. Corrects issue with Exercise form.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -6,11 +6,17 @@ class WorkoutsController < ApplicationController
     end
 
     def create
+        @workout = Workout.new(workout_params)
+        if @workout.save
+            redirect_to user_workouts_path(current_user.id)
+        else
+            render :new
+        end
     end
 
     private
 
     def workout_params
-        params.require(:workout).permit(:user_id, :name)
+        params.require(:workout).permit(:user_id, :name, :routines_attributes)
     end
 end
