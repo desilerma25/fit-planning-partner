441b4bb67ad29ceff30e3c1a4ec723400d463a37
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-19 20:12:01 -0600

    Allows user to now signin through app or Google.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
+
+    def self.from_omniauth(response)
+        User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
+            u.name = response[:info][:name]
+            u.email = response[:info][:email]
+            u.password = SecureRandom.hex(15)
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_025603) do
+ActiveRecord::Schema.define(version: 2021_01_20_011428) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2021_01_17_025603) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
   end
 
   create_table "workouts", force: :cascade do |t|
