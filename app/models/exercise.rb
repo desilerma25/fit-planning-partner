b7ee05c3a4328e516550e6ee59c0b9d7f0e992fd
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-21 16:54:02 -0600

    Adds distinct count and list of workouts an exercise is in on show pg.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,6 @@
 class Exercise < ApplicationRecord
     has_many :routines
-    has_many :workouts, through: :routines
+    has_many :workouts, -> {distinct}, through: :routines
 
     validates :name, uniqueness: { case_sensitive: false }
     validates :name, :target_area, :category, presence: true 
@@ -9,5 +9,8 @@ class Exercise < ApplicationRecord
     scope :order_by_name, -> { order("lower(name) ASC") }
     scope :unique_targets, -> { select(:target_area).distinct }
     scope :unique_categories, -> { select(:category).distinct }
+
+    # scope :workout_count, -> { count(:workout_id) }
+    # scope :unique_workouts, -> { select(:workouts).distinct }
 
 end
