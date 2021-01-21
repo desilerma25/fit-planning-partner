f4a344f60394e4ad9f1faed76beb34be0c8dc5de
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-20 19:26:22 -0600

    Adds flash message if validations for login are not met. Fixes validation issue for login w/ Google.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,14 +13,14 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else 
-            flash[:message] = "Email or Password does not match our records. Please try again."
-            render :new
+            # flash[:error] = "Email or Password does not match our records. Please try again."
+            redirect_to '/login', alert: "Email or Password does not match our records. Please try again."
         end
     end
 
     def destroy
         session.delete :user_id
-        redirect_to :root
+        redirect_to :root, alert: "You have successfully logged out!"
     end
 
     def omniauth
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
     has_many :exercises, through: :workouts
 
     validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :password, length: { minimum: 6 }
+    validates :email, presence: true
+    validates :email, uniqueness: true, on: :create
+    validates :password, length: { minimum: 6 }, on: :create
 
     def self.from_omniauth(response)
         User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
