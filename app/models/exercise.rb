508ba70c59479ef280cbd6fe6dc78091b17bb35c
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-20 20:29:16 -0600

    Adds collection_select for target_area when creating an exercise.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,6 +2,8 @@ class Exercise < ApplicationRecord
     has_many :routines
     has_many :workouts, through: :routines
 
+    # validates 
+
     scope :find_by_target_area, -> (muscle) { where("target_area LIKE ?", "%#{muscle}%")}
     scope :order_by_name, -> { order("lower(name) ASC") }
 end
