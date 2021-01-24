2ce79f8817411cd473bfa8d9f09661f624fff8f2
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-23 18:05:14 -0600

    Adds 404 error for non existing routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,8 @@ include ApplicationHelper
         render '/rescue/no_record_found'
     end
 
+    def route_not_found
+        render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
   resources :workouts do 
     resources :routines, shallow: true
   end
-  resources :users #do 
-    # resources :workouts, shallow: true
-  # end
+  resources :users
+
+  match '*unmatched', to: 'application#route_not_found', via: :all
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
