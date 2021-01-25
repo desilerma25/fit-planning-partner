b5a31a255f98122c43475d919ea45f08bf6e5f9a
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-24 19:59:31 -0600

    Adds bolded styling to listed workouts on index.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,6 +6,7 @@ class ExercisesController < ApplicationController
     end
 
     def create
+        # byebug
         @exercise = Exercise.new(exercise_params)
         if @exercise.save
             redirect_to exercise_path(@exercise)
