b374301719ca5b5bc4e1cd791d23f6da221b518d
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-02 19:05:01 -0600

    DRY's code in routine controller.

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,13 +1,12 @@
 class RoutinesController < ApplicationController
     before_action :redirect_if_not_logged_in
+    before_action :locate_workout
 
     def new
-        @workout = Workout.find(params[:workout_id])
         @routine = @workout.routines.build #need to asscoiate
     end
 
     def create 
-        @workout = Workout.find(params[:workout_id])
         @routine = @workout.routines.build(routine_params)
         if @routine.save
             redirect_to workout_routines_path(@workout)
@@ -17,7 +16,6 @@ class RoutinesController < ApplicationController
     end
 
     def index
-        @workout = Workout.find(params[:workout_id])
         @routines = @workout.routines 
     end
 
@@ -25,6 +23,10 @@ class RoutinesController < ApplicationController
 
     def routine_params
         params.require(:routine).permit(:reps, :sets, :exercise_id)
+    end
+
+    def locate_workout
+        @workout = Workout.find(params[:workout_id])
     end
 
 end
